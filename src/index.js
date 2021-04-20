17e307f150fc074fd1c1aebd1201d47ebcd38af9
sengdu <sengdu005416@gmail.com>
2021-04-20 22:43:58 +0630

    router-fetch-data-array-object

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+// import App from './components/app';
 import reducers from './reducers';
+import { BrowserRouter , Route } from "react-router-dom";
+import PostsIndex from './components/posts_index';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+    
   </Provider>
   , document.querySelector('.container'));
